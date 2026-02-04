d5280bc4cfc1bd64444a737d8bd15c9296f9b9c2
iqchowdhury <38137786+iqchowdhury@users.noreply.github.com>
2026-02-04 02:16:43 +0000

    updated

--- first_snowpark_project/deploy_snowpark_app.py
+++ first_snowpark_project/deploy_snowpark_app.py
@@ -6,7 +6,7 @@ import yaml
 # os.system(f"conda activate snowpark")
 directory_path= sys.argv[1]
 print(f"Directory path: {directory_path}")
-
+##
 # os.chdir(f"{directory_path}")
 # Make sure all 6 SNOWFLAKE_ environment variables are set
 ## SnowCLI accesses the passowrd directly from the SNOWFLAKE_PASSWORD environmnet variable
